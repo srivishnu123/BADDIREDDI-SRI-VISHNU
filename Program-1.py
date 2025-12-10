dabeb1329dd467e07fbf8b82bacd26c68301c21b
sri_vishnu_03 <168799895+srivishnu123@users.noreply.github.com>
2025-12-10 11:28:23 +0530

    Implement basic calculator operations

--- Program-1.py
+++ Program-1.py
@@ -1,1 +1,17 @@
+a=float(input("Enter a: "))
+b=float(input("Enter b: "))
+operation=input("Enter operation:")
+if operation == "add":
+    print("Result:",a+b)
+elif operation=="sub":
+    print("Result:",a-b)
+elif operation=="mul":
+    print("Result:",a*b)
+elif operation=="div":
+    if b==0:
+        print("Error: Division by zero")
+    else:
+        print("Result:",a/b)
+else:
+    print("Invalid Operation")
 
